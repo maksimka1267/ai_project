b87b2273f4b084db176aded7b744182a1a0b536f
Chistykov Maksim <maksim126723@gmail.com>
2025-01-15 23:43:18 +0200

    работа с отрисовкой квадратов, линий, круга и текста

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('image/img.png')# загрузка фото
+photo = np.zeros((450, 450, 3), dtype='uint8')
 
-img =cv2.resize(img, (img.shape[1] // 2, img.shape[1] // 2)) # изменения размеров
-img = cv2.GaussianBlur(img, (9, 9), 0) # размытие
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # отображение одного слоя цвета
+# photo[100:150, 200:280] = 119, 201, 105
 
-img = cv2.Canny(img, 100, 100) #переведение в бинарный формат
+#отрисовка квадрата с зарисованной обвоткой
+cv2.rectangle(photo, (0, 0), (100, 100), (119, 201, 105), thickness=3)
 
-kernel = np.ones((5, 5), np.uint8)
-img = cv2.dilate(img, kernel, iterations=1) # увелечение обвотки
+#отрисовка зарисованого квадрата
+#cv2.rectangle(photo, (0, 0), (100, 100), (119, 201, 105), thickness=cv2.FILLED)
 
-img = cv2.erode(img, kernel, iterations=1)
+#отрисовка линии
+cv2.line(photo, (0,photo.shape[0]//2), (photo.shape[1],photo.shape[0]//2), (119, 201, 105),thickness=3)
 
-cv2.imshow('test-image', img) # вывод картинки
+#отрисовка круга
+cv2.circle(photo, (photo.shape[1]//2, photo.shape[0]//2), 100, (119, 201, 105), thickness=1)
 
-# print(img.shape)
-
-cv2.waitKey(0) # срок отображения
-# cap = cv2.VideoCapture(0)# загрузка видео
-# cap.set(3, 500)# изменения размеров
-# cap.set(4, 500)# изменения размеров
-#
-# while True:
-#     success, img =cap.read()
-#     cv2.imshow('Result',img)# вывод видео
-#
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
+#отрисовка текста
+cv2.putText(photo, 'Hello World!',(100, 150), cv2.FONT_HERSHEY_TRIPLEX,1, (255,0,0), thickness=2)
+cv2.imshow('record', photo)
+cv2.waitKey(0)
