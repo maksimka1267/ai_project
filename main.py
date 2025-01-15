65393f3004540762b832ae6d476b9f6b0fcccbfe
Chistykov Maksim <maksim126723@gmail.com>
2025-01-15 23:20:44 +0200

    work with photo and video

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+img = cv2.imread('image/img.png')# загрузка фото
 
+img =cv2.resize(img, (img.shape[1] // 2, img.shape[1] // 2)) # изменения размеров
+img = cv2.GaussianBlur(img, (9, 9), 0) # размытие
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # отображение одного слоя цвета
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+img = cv2.Canny(img, 100, 100) #переведение в бинарный формат
 
+kernel = np.ones((5, 5), np.uint8)
+img = cv2.dilate(img, kernel, iterations=1) # увелечение обвотки
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+img = cv2.erode(img, kernel, iterations=1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cv2.imshow('test-image', img) # вывод картинки
+
+# print(img.shape)
+
+cv2.waitKey(0) # срок отображения
+# cap = cv2.VideoCapture(0)# загрузка видео
+# cap.set(3, 500)# изменения размеров
+# cap.set(4, 500)# изменения размеров
+#
+# while True:
+#     success, img =cap.read()
+#     cv2.imshow('Result',img)# вывод видео
+#
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
