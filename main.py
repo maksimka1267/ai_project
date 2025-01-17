94e9bd781e4352f05459af88a6e873cbd86f91b1
Chistykov Maksim <maksim126723@gmail.com>
2025-01-18 00:06:23 +0200

    базовое обучение ии по поиску номеров автомобилей

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import cv2
 import numpy as np
-
-img = cv2.imread('image/people_5.png')
+import imutils
+import easyocr
+from matplotlib import pyplot as pl
+from sympy import false
+#открыли и поменяли цвет
+img = cv2.imread('image/number_4.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#натренируем нашу модель
-faces = cv2.CascadeClassifier('faces.xml')
-#получим координаты лиц
-#scaleFactor=2 значит что то что мы ищем может быть в 2 раза больше чем в тренировке
-#minNeighbors=3 насколько много может быть найденых обьектов друг рядом с другом
-results = faces.detectMultiScale(gray, scaleFactor=1.045, minNeighbors=5)
+#оптимизировали его для более быстрой работы и нашли края
+img_filter = cv2.bilateralFilter(gray, 11, 15, 15)
+edges = cv2.Canny(img_filter, 30, 200)
+#нашли контуры
+cont = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cont = imutils.grab_contours(cont)
+#отсортировали и выделили нужные контуры
+cont = sorted(cont, key=cv2.contourArea, reverse=True)
 
-for (x, y, w, h) in results:
-    square = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=2)
+pos = None
+for c in cont:
+    approx = cv2.approxPolyDP(c, 10, True)
+    if len(approx) == 4:
+        pos=approx
+        break
 
-cv2.imshow('Result', img)
+mask = np.zeros(gray.shape, np.uint8)
+new_img = cv2.drawContours(mask, [pos], 0, 255, -1)
+bitwise_img=(cv2.bitwise_and(img, img, mask=mask))
+x, y =np.where(mask == 255)
+x1, y1 = np.min(x), np.min(y)
+x2, y2= np.max(x), np.max(y)
+crop = gray[x1:x2, y1:y2]
 
-cv2.waitKey(0)
+text = easyocr.Reader(['en'])
+text=text.readtext(crop)
+
+res = text[0][-2]
+print(res)
+pl.imshow(cv2.cvtColor(bitwise_img, cv2.COLOR_BGR2RGB))
+
+pl.show()
